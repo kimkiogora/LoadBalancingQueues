e411e429332e2909e4d2fd3fe9f93bb954307e81
Kim Kiogora <kimkiogora@gmail.com>
2016-05-24 13:31:11 +0300

    Update loadbalancer_tests.py
    Added testing class

--- src/loadbalancer_tests.py
+++ src/loadbalancer_tests.py
@@ -1,3 +1,28 @@
 """
 Balancer Unit Tests Class
 """
+#!/usr/bin/env python
+"""
+Author: Kim Kiogora <kimkiogora@gmail.com>
+Usage : Sample Impl of a load balancing lists (dummy queues)
+"""
+from LoadBalancer import LoadBalancer
+
+queue_n1=[10,10,10,10]
+queue_n2=[10]
+queue_n3=[10,10,10,10,10,10]
+
+
+all_queues = []
+all_queues.append(queue_n1)
+all_queues.append(queue_n2)
+all_queues.append(queue_n3)
+
+print "All queues"
+print all_queues
+
+load_balancer = LoadBalancer()
+result = load_balancer.balance(all_queues);
+
+print "Balanced Queues"
+print result
