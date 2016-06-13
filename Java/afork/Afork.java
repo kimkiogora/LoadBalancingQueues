a4a09a5c2ac6ed7e18f7075128fd2d341d94dc54
Kim Kiogora <kimkiogora@gmail.com>
2016-06-13 10:07:19 +0300

    Create Afork.java

---
+++ Java/afork/Afork.java
@@ -0,0 +1,18 @@
+/**
+ * Author : Kim Kiogora Usage : Apache way of forking processes for High
+ * Performance Applications that require high throughput
+ */
+package afork;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kim kiogora
+ */
+public class Afork extends Thread {
+}
